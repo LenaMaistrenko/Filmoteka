dee28d5461325c1760a1d804d643f2584ee11ff0
GalinaBespiata <116351519+GalinaBespiata@users.noreply.github.com>
2023-01-19 12:58:00 +0200

    for clonne

--- src/js/search.js
+++ src/js/search.js
@@ -1,3 +1,4 @@
 import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Pagination from 'tui-pagination';
+import 'tui-pagination/dist/tui-pagination.min.css';
