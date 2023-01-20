5af1725416d9d448a8f6e08e2a3fecc3e7921d6a
Viktoriia38 <bva922@ukr.net>
2023-01-20 20:35:52 +0200

    Made modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -2,3 +2,17 @@ import axios from 'axios';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.min.css';
+
+const refs = {
+  openModalBtn: document.querySelector('[data-open-modal]'),
+  closeModalBtn: document.querySelector('[data-close-modal]'),
+  backdrop: document.querySelector('[data-backdrop]'),
+};
+
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  document.body.classList.toggle('data-open-modal');
+  refs.backdrop.classList.toggle('is-hidden');
+}
