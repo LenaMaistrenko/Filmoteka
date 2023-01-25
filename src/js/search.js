73dfa10f88394734a1aa785dfcb9acc32ac495ab
LenaMaistrenko <istlauder@gmail.com>
2023-01-25 19:24:00 +0200

    +
    футер, скрол до верху

--- src/js/search.js
+++ src/js/search.js
@@ -21,24 +21,27 @@ async function searchFilm(event) {
     elements: { search },
   } = event.currentTarget;
   searched = search.value;
-  
+
   if (search.value.length < 2) {
     Notify.warning('Its name too short.Enter the correct movie name, please.');
     event.currentTarget.reset();
+    searchInput.value = '';
     return;
   }
   try {
     const { results, page, total_pages } = await getByName(searched.trim(), 1);
-   
+
     const cardsList = document.querySelector('.cards__list');
-    // if (cardsList) {
-    //   cardsList.innerHTML = '';
-    // }
+    if (cardsList) {
+      cardsList.innerHTML = '';
+    }
     if (Array.isArray(results) && results.length) {
       renderMarkup(results);
       pagination(1, total_pages);
     } else {
       notification.classList.remove('close');
+      searchInput.value = '';
+      pagination(0, 0);
     }
   } catch (error) {
     console.log(error);
@@ -48,7 +51,6 @@ export async function getByName(name, page) {
   const { data } = await axios.get(
     `https://api.themoviedb.org/3/search/movie?api_key=004aa31770cc2729c6dd319813b8b5dc&query=${name}&page=${page}`
   );
-  
 
   return data;
 }
